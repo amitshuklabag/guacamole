6dea7459df0f166790124a7491aedd5d93119444
Amit Shukla <53058928+amitshuklabag@users.noreply.github.com>
2023-06-12 19:52:33 +0530

    Initial commit

---
+++ scripts/preInstall.sh
@@ -0,0 +1,9 @@
+mkdir -p ./init
+mkdir -p ./mysql
+mkdir -p ./drive
+mkdir -p ./record
+
+chown -R 1001:1001 ./init
+chown -R 1001:1001 ./mysql
+chown -R 1001:1001 ./drive
+chown -R 1001:1001 ./record
